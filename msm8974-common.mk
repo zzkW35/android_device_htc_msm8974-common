8ad9e688258e595d26bf88b62c9c92b5337084cb
Filippo <zzkw35@gmail.com>
2020-03-02 14:40:43 +0100

    StatiX-ify

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -23,8 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
@@ -135,7 +134,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     libshim_camera \
-    Snap
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -272,10 +270,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermanager.xml:system/vendor/etc/thermanager.xml
-
-# Touch HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.htc_msm8974
 
 # USB
 PRODUCT_PACKAGES += \
