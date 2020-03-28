67b965052922fbe0463f70b306c2fafdd763a35e
zzkW35 <zzkw35@gmail.com>
2020-03-28 01:37:06 +0100

    msm8974-common: Build telephony packages

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -265,6 +265,19 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Thermal
 PRODUCT_PACKAGES += \
     thermanager
